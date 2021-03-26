3d88052bbb2181968772eac78a2eaa882ac63946
OksanaTarun <oxana.tarun@gmail.com>
2021-03-26 10:13:09 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateIfRemain1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1500);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfLessThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(400);
+        int expected = 600;
+        assertEquals(actual, expected);
+    }
+
+
+
+}
